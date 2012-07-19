5c7b9b2a586d2164725479194ebf305272f9f63f
bling <bling@live.ca>
2012-07-18 21:15:59 -0400

    initial commit

---
+++ ResharperStaticResource/SharedResourceDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ResharperStaticResource
+{
+    /// <summary>
+    /// http://wpftutorial.net/MergedDictionaryPerformance.html
+    /// </summary>
+    public class SharedResourceDictionary : ResourceDictionary
+    {
+    }
+}
